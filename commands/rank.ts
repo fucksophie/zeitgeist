6fc374f785747913c2716c9d4368d40e3cfd8cc6
yourfriend <friend@yourfriend.lv>
2022-07-28 00:07:27 +0300

    proper

--- commands/rank.ts
+++ commands/rank.ts
@@ -15,6 +15,6 @@ export default function (player: Player, client: Client, args: string[])  {
     } else {
         const dPlayer: DatabasePlayer = JSON.parse(localStorage.getItem(client.wsUrl+player.id)!);
 
-        client.message(args[0] + "'s rank is: " + (dPlayer.rank ||"(none)")+ ". Room rank: " + (dRoom.ranks.get(args[0]) || "(none)"))
+        client.message(player.name + "'s rank is: " + (dPlayer.rank ||"(none)")+ ". Room rank: " + (dRoom.ranks.get(args[0]) || "(none)"))
     }
 }
