2244e894dfa293dff437c552d6c0591e6b70e807
yourfriend <friend@yourfriend.lv>
2022-07-27 18:32:47 +0300

    legacy code fp

--- commands/crown.ts
+++ commands/crown.ts
@@ -1,10 +1,11 @@
 import { Client, Player } from "../Client.ts";
-import { DatabasePlayer } from "../Database.ts";
+import { DatabasePlayer, DatabaseRoom, getDRoom } from "../Database.ts";
 
 export default function (player: Player, client: Client)  {
     const dPlayer: DatabasePlayer = JSON.parse(localStorage.getItem(client.wsUrl+player.id)!);
+    const dRoom: DatabaseRoom = getDRoom(client)!;
 
-    if(dPlayer.rank == "room-owner" || dPlayer.rank == "bot-owner") {
+    if(dRoom.ranks.get(player.id) == "room-owner" || dPlayer.rank == "bot-owner") {
         if(!client.me.crown) {
             client.message("I do not have the crown.")
             return;
