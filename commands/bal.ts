2cdc66f8b88db6a90ba7298adf0cb1af763da282
yourfriend <friend@yourfriend.lv>
2022-07-27 16:56:25 +0300

    fix bug

--- commands/bal.ts
+++ commands/bal.ts
@@ -1,5 +1,5 @@
 import { Client, Player } from "../Client.ts";
 
 export default function (player: Player, client: Client)  {
-    client.message(`@${player.id} has ${JSON.parse(localStorage.getItem(player.id)!).money}$#.`)
+    client.message(`@${player.id} has ${JSON.parse(localStorage.getItem(client.wsUrl+player.id)!).money}$#.`)
 }
