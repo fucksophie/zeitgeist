d4cce53cb2ea8cdf19bbb1da0e4d052058f74fc5
yourfriend <friend@yourfriend.lv>
2022-07-27 17:38:48 +0300

    fix tons of buggess

--- commands/setrank.ts
+++ commands/setrank.ts
@@ -20,12 +20,40 @@ export default function (player: Player, client: Client, args: string[])  {
                     return;
                 } 
 
+                if(dRoom.banned.includes(args[0])) {
+                    client.message("User is banned.")
+                    return;
+                }
+
+                if(dRoom.owners.includes(args[0])) {
+                    client.message("User is already a room owner!");
+                    return;
+                }
+
+                if(dRoom.operators.includes(args[0])) {
+                    dRoom.operators = dRoom.operators.filter(e => e !== args[0])
+                }
+                
                 dRoom.owners.push(args[0]);
 
                 localStorage.setItem("room_"+client.wsUrl+client.channel, JSON.stringify(dRoom));
 
                 client.message(args[0] + " is now a room owner.");
             } else if(args[1] == "room-operator") {
+                if(dRoom.operators.includes(args[0])) {
+                    client.message("User is already a room operator!");
+                    return;
+                }
+
+                if(dRoom.banned.includes(args[0])) {
+                    client.message("User is banned.")
+                    return;
+                }
+
+                if(dRoom.owners.includes(args[0])) {
+                    dRoom.owners = dRoom.owners.filter(e => e !== args[0])
+                }
+
                 dRoom.operators.push(args[0]);
 
                 localStorage.setItem("room_"+client.wsUrl+client.channel, JSON.stringify(dRoom));
