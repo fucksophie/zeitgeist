4f5541559b2eddef714ae370cdd2459359d08dc1
yourfriend <friend@yourfriend.lv>
2022-08-07 12:52:10 +0300

    remove lapisstatus, move wordrace to economy, plan namehistory and togglepiano

--- commands/experimental/lapisstatus.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Player } from "../../classes/Client.ts";
-
-export default function (_: Player, client: Client) {
-  client.message(
-    "Current lapis status: " +
-      (localStorage.getItem("lapis") || "hasn't been set yet"),
-  );
-}
