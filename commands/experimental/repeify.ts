8c3543e3f25eba99c0d2ff2bbc605ba00ae3d0eb
yourfriend <friend@yourfriend.lv>
2022-08-10 19:42:26 +0300

    reepify more testing

--- commands/experimental/repeify.ts
+++ commands/experimental/repeify.ts
@@ -3,12 +3,12 @@ import { Client, Player } from "../../classes/Client.ts";
 export default function (_: Player, client: Client, args: string[]) { 
     const repeified = args.join(" ").replaceAll("g", "r").replaceAll("l", "p").replaceAll("d", "e");
     
-    if(repeified.split(" ").join("").includes("rape")) {
+    if(repeified.split(" ").join("").toLowerCase().includes("rape")) {
         client.message("No. (1)");
         return;
     }
     
-    if(!/^[a-zA-z0-9 ]*$/gm.test(repeified.split(" ").join(""))) {
+    if(!/^[a-zA-z0-9 ]*$/gm.test(repeified.split(" ").join("").toLowerCase())) {
         client.message("No. (2)")
         return;
     }
