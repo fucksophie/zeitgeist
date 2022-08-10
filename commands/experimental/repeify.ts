66981149284c04571916f4e8eb32e78282138f52
yourfriend <friend@yourfriend.lv>
2022-08-10 19:41:06 +0300

    more obscurite detection

--- commands/experimental/repeify.ts
+++ commands/experimental/repeify.ts
@@ -2,9 +2,16 @@ import { Client, Player } from "../../classes/Client.ts";
 
 export default function (_: Player, client: Client, args: string[]) { 
     const repeified = args.join(" ").replaceAll("g", "r").replaceAll("l", "p").replaceAll("d", "e");
+    
     if(repeified.split(" ").join("").includes("rape")) {
-        client.message("No.");
+        client.message("No. (1)");
         return;
     }
+    
+    if(!/^[a-zA-z0-9 ]*$/gm.test(repeified.split(" ").join(""))) {
+        client.message("No. (2)")
+        return;
+    }
+
     client.message("Repeified: " + repeified)
 }
