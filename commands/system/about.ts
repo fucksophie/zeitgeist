3be7a28e7bcaf3fa20ce055cc94e130e02441019
yourfriend <friend@yourfriend.lv>
2022-10-09 16:05:50 +0300

    add code link (link to code)

--- commands/system/about.ts
+++ commands/system/about.ts
@@ -5,6 +5,6 @@ export default function (_: Player, client: Client) {
   client.message(
     `Bot written as a replacement for Bouncer and MelonBot, moderating ${config.servers.length} servers, ${
       config.servers.map((e) => e.rooms.length).reduce((a, b) => a + b)
-    } rooms. Created by ~yourfriend, input from Lapis and Wolfy. Zeit[g]eist (Database Rewrite)`,
+    } rooms. Created by ~yourfriend, input from Lapis and Wolfy. Code: https://github.com/yourfriendoss/zeitgeist. Zeit[g]eist (Database Rewrite)`,
   );
 }
